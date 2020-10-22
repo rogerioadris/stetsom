9b5d58290ed0ea01dea5b91fc9b769bbc40987cf
Rogério Adriano <rogerioadris@gmail.com>
2020-10-22 15:54:42 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,30 @@
 importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-messaging.js');
+
 firebase.initializeApp({
-    messagingSenderId: "229011201122"
+    apiKey: "AIzaSyAPvKFh3NKplqoRFZA3lBlkz7mFdNxD_rs",
+    authDomain: "stb-events.firebaseapp.com",
+    databaseURL: "https://stb-events.firebaseio.com",
+    projectId: "stb-events",
+    storageBucket: "stb-events.appspot.com",
+    messagingSenderId: "229011201122",
+    appId: "1:229011201122:web:bd61292f6cd835ef6ac53b"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onMessage((payload) => {
+    console.log('Message received. ', payload);
+    // ...
+});
+
+messaging.setBackgroundMessageHandler(function (payload) {
+    console.log('[firebase-messaging-sw.js] Received background message ', payload);
+    const notificationTitle = 'Background Message Title';
+    const notificationOptions = {
+        body: 'Background Message body.',
+        icon: '/firebase-logo.png'
+    };
+
+    return self.registration.showNotification(notificationTitle, notificationOptions);
 });
