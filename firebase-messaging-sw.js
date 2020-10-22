90d863c04d6232461fb6bd8671a86130a2fc8271
Rogério Adriano <rogerioadris@gmail.com>
2020-10-22 15:47:52 -0300

    Create firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,5 @@
+importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.24.0/firebase-messaging.js');
+firebase.initializeApp({
+    messagingSenderId: "229011201122"
+});
